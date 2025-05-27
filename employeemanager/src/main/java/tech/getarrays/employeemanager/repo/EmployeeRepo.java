6feda0e3760501ad7e2c83ed290fd1f68f65b0dd
Justin Penny <justincpenny@hotmail.com>
2025-05-26 21:26:13 -0400

    small update. Seems I didn't realy have a syntax issue before

--- employeemanager/src/main/java/tech/getarrays/employeemanager/repo/EmployeeRepo.java
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -5,4 +5,11 @@ import tech.getarrays.employeemanager.model.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long>{
 
+    // These functions were created by the IDE by attemping to call employeeRepo.findEmployeeById(id); in the EmployeeService.java and then allowing
+    // the IDE to quick fix the issue. Spring can tell based on naming convention what should be created to fix the issue
+    // Look up JPA Query Method creation for more info
+    void deleteEmployeeById(Long id);
+
+    Employee findEmployeeById(Long id);
+
 }
--- employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package tech.getarrays.employeemanager.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,24 @@ public class EmployeeService {
         this.employeeRepo = employeeRepo;
     }
 
-    public Employee addEmployee(Employee employee){
+    public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee (Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id);
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
