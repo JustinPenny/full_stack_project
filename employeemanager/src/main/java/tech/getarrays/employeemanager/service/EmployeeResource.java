acd757ca35865345f0770277d8fd23554e24b938
Justin Penny <justincpenny@hotmail.com>
2025-05-29 22:14:48 -0400

    Fleshing out service. Tested with data inserts

--- employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeResource.java
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeResource.java
@@ -2,8 +2,12 @@ package tech.getarrays.employeemanager.service;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,8 @@ import tech.getarrays.employeemanager.model.Employee;
 
 import java.util.List;
 
+// This mirrors the EmployeeService file.
+// The HTTP requests will land here and from here call the respective methods from EmployeeService
 
 @RestController
 @RequestMapping("/employee")
@@ -22,16 +28,36 @@ public class EmployeeResource {
         this.employeeService = employeeService;
     }
     
+
+    // Find all employees
     @GetMapping("/all")
-    // Double generic
+    // Double generic so we have to define list and what's in the list
     public ResponseEntity<List<Employee>> getAllEmployee (){
         List<Employee> employee = employeeService.findAllEmployees();
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
-
+    // Find one employee
     @GetMapping("/find/{id}")
     public ResponseEntity<Employee> getAllEmployee (@PathVariable("id") Long id){
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
