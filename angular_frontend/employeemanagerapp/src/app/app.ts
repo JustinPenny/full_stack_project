a6b347b2673e956fc3df7580800f31c1c89f1292
Justin Penny <justincpenny@hotmail.com>
2025-06-04 21:55:04 -0400

    adding employee service to app component

--- angular_frontend/employeemanagerapp/src/app/app.ts
+++ angular_frontend/employeemanagerapp/src/app/app.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { error } from 'console';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'employeemanagerapp';
+  public employees: Employee[] = []; // This might be wrong. Was getting uninitialized error
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(){
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
 }
