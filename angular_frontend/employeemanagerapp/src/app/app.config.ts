2bbf511258a1ea5e60a46dadc12cb2509d39c28b
Justin Penny <justincpenny@hotmail.com>
2025-06-10 00:05:06 -0400

    working out kinks with connecting to backend

--- angular_frontend/employeemanagerapp/src/app/app.config.ts
+++ angular_frontend/employeemanagerapp/src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient(),
+    HttpClientModule, // Depreciated but it's in the tutorial I'm following so it stays for now
+    EmployeeService
   ]
 };
